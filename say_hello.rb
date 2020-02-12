c5e92d7e23ddbc60d0326ef9f2ad13f7dfa9da96
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-12 19:41:57 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,9 @@
 # Build yor say_hello method helper_method
 def say_hello (name)
-  puts "Hello Kent Beck"
+  puts "Hello Kent Beck!"
 end
 
 def say_hello (name = "Ruby Programmer")
-  puts ("Hello, #{name}!")
+  puts ("Hello #{name}!")
 end
   
